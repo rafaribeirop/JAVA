555c8d85ff88533150b3fd5399df76eace8997b7
Rafaella Ribeiro <rafaellaribeirop07@gmail.com>
2026-02-24 17:36:28 -0300

    Create Operadores.java

---
+++ ComandoEntrada/Operadores.java
@@ -0,0 +1,43 @@
+package ComandoEntrada;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+    /* int a = 8;
+        int b = 2;
+       resultado = a++ + b;
+       System.out.println(resultado);
+     */
+
+       /* 
+       int resultado = (int) Math.pow(5, 2);
+       System.out.println(resultado); */
+
+       /* ARREDONDAMENTO
+       double ab, n, n1, n2;
+
+       ab = Math.abs(-5);
+       System.out.println(ab);
+
+       n = Math.round(3.3);
+       System.out.println(n);
+
+       n1 = Math.floor(9.9);
+       System.out.println(n1);
+
+       n2 = Math.ceil(6.1);
+       System.out.println(n2); */
+
+       int r = (int) 0 + Math.random() * (9-0);
+       System.out.println(r);
+
+
+
+    
+
+        
+
+
+        
+    }
+}
