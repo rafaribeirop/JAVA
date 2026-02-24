81e472d547dfa2c400151950e6b19490d0664da6
Rafaella Ribeiro <rafaellaribeirop07@gmail.com>
2026-02-24 15:47:42 -0300

    Update Texto.java

--- ComandoEntrada/Texto.java
+++ ComandoEntrada/Texto.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class Texto {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        String texto, palavra;
+        String nome, palavra, texto;
 
-        System.out.println("Tetse");
+        System.out.println("Digite seu nome completo: ");
+        nome = entrada.nextLine();
+        nome = nome.trim();
+        nome = nome.toUpperCase();
+        System.out.println("Digite uma palavra: ");
+        palavra = entrada.next();
+        palavra = palavra.toLowerCase();
+        System.out.println("Digite seu texto: ");
 
+        System.out.println(nome);
+        System.out.println(palavra);
+        entrada.close();
     }
 }
