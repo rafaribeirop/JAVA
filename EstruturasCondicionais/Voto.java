9542de2f35f398092f56d95eb311cd2b585cb402
Rafaella Ribeiro <rafaellaribeirop07@gmail.com>
2026-02-25 17:04:29 -0300

    Create Voto.java

---
+++ EstruturasCondicionais/Voto.java
@@ -0,0 +1,25 @@
+package EstruturasCondicionais;
+import java.util.Scanner;
+public class Voto {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int idade;
+
+        System.out.println("Qual sua idade? ");
+        idade = in.nextInt();
+        System.out.println("Sua idade é " + idade);
+
+        if (idade < 16) {
+            System.out.println("Você não vota !");
+        } else {
+            if ((idade >= 16 && idade < 18) || (idade > 70)) {
+                System.out.println("Voto opcional! ");
+            } else {
+                System.out.println("Voto obrigatório!");
+            }
+        }
+        in.close();
+        
+    }
+}
