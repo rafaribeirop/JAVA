6e5cd1fe74a1f640069c5386619241d5c9a75cfa
Rafaella Ribeiro <rafaellaribeirop07@gmail.com>
2026-02-27 19:43:29 -0300

    for

---
+++ EstruturasDeRepeticao/Tabuada.java
@@ -0,0 +1,21 @@
+package EstruturasDeRepeticao;
+
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        int contador;
+
+        System.out.println("Quer ver a tabuada de qual número: ");
+        numero = entrada.nextInt();
+        System.out.println("------");
+
+        for(contador = numero; contador <= (numero*10); contador += numero){
+            System.out.println(contador);
+        }
+        entrada.close();
+    }
+}
