00ea72de3a3da772573c23d17b5c7bbd69fc915e
Rafaella Ribeiro <rafaellaribeirop07@gmail.com>
2026-02-28 16:30:31 -0300

    Create CalculadoraSimples.java

---
+++ Desafios/CalculadoraSimples.java
@@ -0,0 +1,64 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class CalculadoraSimples {
+    public static void main(String[] args) {
+
+        Scanner entrada= new Scanner(System.in);
+        int resposta;
+        String continuar;
+
+        do {
+            System.out.println("===============================");
+            System.out.println("|     Calculadora Simples     |");
+            System.out.println("===============================");
+            System.out.println("| Qual operação deseja fazer? |");
+            System.out.println("| [1] Para somar (+)          |");
+            System.out.println("| [2] Para subtrair (-)       |");
+            System.out.println("| [3] Para multiplicar (x)    |");
+            System.out.println("| [4] Para dividir (/)        |");
+            System.out.println("| [5] Para sair               |");
+            System.out.println("===============================");
+            resposta = entrada.nextInt();
+        
+            if (resposta >= 1 && resposta <= 4) {
+                int n1, n2;
+
+                System.out.println("Digite o 1o número: ");
+                n1 = entrada.nextInt();
+                System.out.println("Digite o 2o número: ");
+                n2 = entrada.nextInt();
+
+                double resultado;
+
+                if (resposta == 1){  
+                    resultado = n1 + n2;
+                    System.out.println(n1 + " + " + n2 + " = " + resultado); 
+                } else if (resposta == 2){
+                    resultado = n1 - n2;
+                    System.out.println(n1 + " - " + n2 + " = " + resultado); 
+                } else if (resposta == 3){
+                    resultado = n1 * n2;
+                    System.out.println(n1 + " x " + n2 + " = " + resultado); 
+                } else if (resposta == 4){
+                    resultado = (double) n1 / n2;
+                    System.out.println(n1 + " / " + n2 + " = " + resultado); 
+                } 
+
+            } else if (resposta == 5){
+                    System.out.println("..Saindo...");
+                    break;
+            } else {
+                System.out.println("Digito inválido");
+            }
+
+            System.out.println("Deseja calcular novamente? [S/N]");
+            continuar = entrada.next();
+
+        } while (continuar.equalsIgnoreCase("S"));
+        
+        entrada.close();
+ 
+    }
+}
