386e822108ba227d050cce6491b0c3eec4a67b04
Rafaella Ribeiro <rafaellaribeirop07@gmail.com>
2026-03-02 10:28:15 -0300

    metodos

---
+++ Vetores/Busca.java
@@ -0,0 +1,17 @@
+package Vetores;
+import java.util.Arrays;
+
+public class Busca {
+    public static void main(String[] args) {
+        int n[] = {3,7,1,2,5};
+        int p = Arrays.binarySearch(n, 1);
+        Arrays.sort(n);
+
+        System.out.println("O valor 1 está na posição "+ p);
+
+        for(int valor: n){
+            System.out.println(valor);    
+        }
+
+    }
+}
---
+++ Vetores/Preencher.java
@@ -0,0 +1,17 @@
+package Vetores;
+
+import java.util.Arrays;
+
+public class Preencher {
+    public static void main(String[] args) {
+
+        int v[] = new int [100];
+        Arrays.fill(v, 1);
+
+        for(int valor: v){
+            System.out.print(valor + " ");
+
+        }
+    }
+    
+}
