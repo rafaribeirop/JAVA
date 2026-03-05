acc54068a047984e4f52a468dbbc2181fe4edef5
Rafaella Ribeiro <rafaellaribeirop07@gmail.com>
2026-03-05 13:38:58 -0300

    a

---
+++ src/poo/classesobjetos/Caneta.java
@@ -0,0 +1,33 @@
+package classesobjetos;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    double ponta;
+    int tinta;
+    boolean tampada;
+
+    void Status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Tampada: " + this.tampada);
+        System.out.println("Carga: " + this.tinta);
+        System.out.println();
+    }
+
+    void rabiscar(){
+
+    }
+
+    void tampar(){
+
+    }
+
+    void destampar(){
+        
+    }
+}
+
+    
+
